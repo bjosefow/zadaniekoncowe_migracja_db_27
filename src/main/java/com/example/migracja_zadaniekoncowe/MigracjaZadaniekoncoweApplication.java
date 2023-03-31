27c086b59a8f5e225b9a4369c407cb681c193d5a
Betina Josefowicz <betina.josefowicz@gmail.com>
2023-03-31 13:16:50 +0200

    init

---
+++ src/main/java/com/example/migracja_zadaniekoncowe/MigracjaZadaniekoncoweApplication.java
@@ -0,0 +1,13 @@
+package com.example.migracja_zadaniekoncowe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigracjaZadaniekoncoweApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MigracjaZadaniekoncoweApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/migracja_zadaniekoncowe/MigracjaZadaniekoncoweApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.migracja_zadaniekoncowe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigracjaZadaniekoncoweApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
